ff9a88241028fe136150d0e05a6782f645156950
George Hotz <geohot@gmail.com>
2015-12-07 12:24:07 -0800

    ALL 6 CHANNELS

--- control.py
+++ control.py
@@ -11,6 +11,8 @@ cnt = 0
 while 1:
   inten = [0]*6
   inten[cnt >> 8] = cnt&0xFF
+  inten[((cnt >> 8)+1)%3 + 3] = cnt&0xFF
+
   handle.bulkWrite(1, ''.join(map(chr, inten)))
   time.sleep(0.01)
   cnt += 4
